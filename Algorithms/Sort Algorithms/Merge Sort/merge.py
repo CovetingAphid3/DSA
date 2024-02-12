c98a9b81fae361245b1cddd70322f624777a763f
CovetingAphid3 <tadiwachawanda@gmail.com>
2024-02-12 21:01:26 +0200

    implement merge sort in python

---
+++ Algorithms/Sort Algorithms/Merge Sort/merge.py
@@ -0,0 +1,35 @@
+
+
+start_time = time.time()
+
+def merge(arr):
+
+    if len(arr) == 1:
+        return arr
+
+    middle = len(arr) // 2
+    leftHalf = arr[0:middle]
+    rightHalf = arr[middle:]
+
+    return _merge(merge(leftHalf),merge(rightHalf))
+
+
+def _merge(left,right):
+    result = []
+    rightIndex = 0
+    leftIndex = 0
+
+    while leftIndex < len(left) and rightIndex < len(right):
+        if left[leftIndex] < right[rightIndex]:
+            result.append(left[leftIndex])
+            leftIndex += 1 
+        else:
+            result.append(right[rightIndex])
+            rightIndex += 1    
+
+    result += left[leftIndex:]
+    result += right[rightIndex:]
+
+    return result
+
+              
