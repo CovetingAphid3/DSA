bd68923a051bac1c00f659d464a77f12fc585d87
CovetingAphid3 <tadiwachawanda@gmail.com>
2024-02-15 19:25:40 +0200

    added methods for bst in python

--- Data-Structures/Binary-Search-Tree/bst.py
+++ Data-Structures/Binary-Search-Tree/bst.py
@@ -22,6 +22,34 @@ class Tree:
             currentNode.right = self._insert(currentNode.right,value)
         return currentNode            
     #delete
+    def delete(self, value):
+        self.root = self._delete(self.root, value)
+
+    def _delete(self, node, value):
+        if not node:
+            return None
+        if value == node.value:
+            # case 1: node with only one child or no child
+            if not node.left:
+                return node.right
+            elif not node.right:
+                return node.left
+
+            # case 2: node with two children
+            node.value = self._find_min(node.right).value
+            node.right = self._delete(node.right, node.value)
+        elif value < node.value:
+            node.left = self._delete(node.left, value)
+        elif value > node.value:
+            node.right = self._delete(node.right, value)
+
+        return node
+
+    def _find_min(self, node):
+        while node.left:
+            node = node.left
+        return node
+
 
     #max
     def max(self):
@@ -58,15 +86,81 @@ class Tree:
     #Traversal
     #BFS
     def BFS(self):
-        pass
-    #
-    #
+        if not self.root:
+            return []
+        queue = []
+        result = []
+        
+        queue.append(self.root)
 
-t = Tree()
-t.insert(10)
-t.insert(3)
-t.insert(11)
-print(t.find(3))
-print(t.min())
-print(t.max())
+        while len(queue):
+            currentNode = queue.pop(0)
+            result.append(currentNode.value)
+
+            if currentNode.left:
+                queue.append(currentNode.left)
+            if currentNode.right:
+                queue.append(currentNode.right)    
+        return result    
+    #DFS
+    #pre_order
+    def pre_order(self):
+        result = []
+        if not self.root:
+            return
+    
+        def walk(currentNode):
+            
+            result.append(currentNode.value)
+            if currentNode.left:
+                walk(currentNode.left)
+            if currentNode.right:
+                walk(currentNode.right)    
+
+        walk(self.root)
+        return result    
+    #post_order
+    def post_order(self):
+        if not self.root:
+            return
+        result = []
+
+        def walk(currentNode):
+            if currentNode.left:
+                walk(currentNode.left)
+            result.append(currentNode.value)
+            if currentNode.right:
+                walk(currentNode.right)
+        walk(self.root)
+        return result        
+
+    #in_order
+    def in_order(self):
+        if not self.root:
+            return
+        result = []
+
+        def walk(currentNode):
+            if currentNode.left:
+                walk(currentNode.left)
+            if currentNode.right:
+                walk(currentNode.right)
+            result.append(currentNode.value)
+        walk(self.root)
+        return result        
+
+    #height
+    def height(self):
+        return self._height(self.root)
+    def _height(self,currentNode):
+        if not currentNode:
+            return 0
+        
+        leftHeight = self._height(currentNode.left)
+        rightHeight = self._height(currentNode.right)
+        
+        height = max(leftHeight,rightHeight) + 1
+        return height
+
+
 
