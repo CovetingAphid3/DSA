49d3b321872b744bb222118e74de290a2ac0db55
Tadiwa Chawanda <123959725+CovetingAphid3@users.noreply.github.com>
2024-02-20 21:28:21 +0200

    Add files via upload

---
+++ Algorithms/Search Algorithm/Linear Search/LinearSearch.cs
@@ -0,0 +1,15 @@
+class Linear
+{
+    static bool LinearSearch(int[] array, int value)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == value)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
