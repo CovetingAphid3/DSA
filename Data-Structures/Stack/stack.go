a365d74020579b6c573f395f1c129c99605009c3
CovetingAphid3 <tadiwachawanda@gmail.com>
2024-03-08 22:58:13 +0200

    stack in go

---
+++ Data-Structures/Stack/stack.go
@@ -0,0 +1,62 @@
+package stack
+
+import "fmt"
+
+type Node struct {
+	Value int
+	Next  *Node
+}
+type Stack struct {
+	Top *Node
+}
+
+func Push(s *Stack, value int) {
+	node := &Node{
+		Value: value,
+		Next:  s.Top,
+	}
+	s.Top = node
+}
+
+func Pop(s *Stack) (int, error) {
+
+	if s.Top == nil {
+		return 0, fmt.Errorf("Stack is empty")
+	}
+	value := s.Top.Value
+	s.Top = s.Top.Next
+
+	return value, nil
+}
+
+func Peek(s *Stack) (int, error) {
+	if s.Top == nil {
+		return 0, fmt.Errorf("Stack is empty")
+	}
+	return s.Top.Value, nil
+}
+
+func main() {
+	mystack := Stack{}
+
+	// Example usage:
+	Push(&mystack, 42)
+	Push(&mystack, 15)
+
+	topValue, err := Peek(&mystack)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Top of the stack:", topValue)
+	}
+
+	poppedValue, err := Pop(&mystack)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Popped value:", poppedValue)
+	}
+
+	fmt.Println("Is the stack empty?", mystack.Top == nil)
+
+}
